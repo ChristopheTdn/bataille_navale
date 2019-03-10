599835406fbdfdff589e6ed2d21e3d80d4efe5bc
GToF <Christophe.Taudin@gmail.com>
2019-03-10 16:50:33 +0100

    pep8 sur depot principal

--- bataille_navale.py
+++ bataille_navale.py
@@ -7,13 +7,15 @@ Auteur : ToF
 years : 2019
 """
 
-#class
+# class
+
+
 class Core():
     """
      Creation du coeur de jeu Bataille navale
 
     """
-    def __init__(self,largeur=10,hauteur=10):
+    def __init__(self, largeur=10, hauteur=10):
         self.grille_cpu_bateau = dict()
         self.grille_cpu_cherche = dict()
         self.grille_joueur_bateau = dict()
@@ -25,10 +27,9 @@ class Core():
         self.hauteur = hauteur
         # rempli les mers de vide (".")
         self.init_grilles()
-        #place les bateaux du CPU et du joueur
+        # place les bateaux du CPU et du joueur
         self.crea_grille(self.grille_cpu_bateau, self.list_bateaux_cpu)
         self.crea_grille(self.grille_joueur_bateau, self.list_bateaux_joueur)
-
 
     def init_grilles(self):
         for j in range(1, self.hauteur+1):
@@ -38,7 +39,7 @@ class Core():
                 self.grille_joueur_bateau[(i, j)] = "."
                 self.grille_joueur_cherche[(i, j)] = "."
 
-    def crea_grille(self,grille,liste):
+    def crea_grille(self, grille, liste):
         bateau1 = Bateau("P", "Porte-Avions", 6)
         bateau1.place_bateau(grille, self.largeur, self.hauteur)
         bateau2 = Bateau("D", "Destroyer", 5)
@@ -57,11 +58,11 @@ class Core():
 
     def affiche(self, grille, grille2=None):
         """Affiche la grille passée en parametre
-        
+
         Arguments:
             grille {dict} -- Un dictionnaire au format (A,1) = "." pour la mer
         """
-        print('\x1b[2J')  #cls
+        print('\x1b[2J')  # cls
         # GRILLE 1
         print(GREEN + "   | ", end="")
         for i in self.largeur_string:
@@ -71,65 +72,67 @@ class Core():
         print(GREEN + "   | ", end="")
         for i in self.largeur_string:
             print(GREEN + " " + i + " ", end="")
-            
+
         print("")
         # GRILLE 1
         print(GREEN + "---|-", end="")
         for i in self.largeur_string:
             print(GREEN + "---", end="")
         print("   ", end="")
+
         # GRILLE 2
         print(GREEN + "---|-", end="")
         for i in self.largeur_string:
             print(GREEN + "---", end="")
-            
+
         for j in range(self.hauteur):
             print("")
-            print (GREEN + "{0:2d}".format(j + 1)+" | ",end="")
+            print(GREEN + "{0:2d}".format(j + 1)+" | ", end="")
             for i in self.largeur_string:
-                couleur= RED               
-                if grille[(i, j + 1)]=="X" :
+                couleur = RED
+                if grille[(i, j + 1)] == "X":
                     couleur = BLUE
-                if grille[(i, j + 1)]=="." :
+                if grille[(i, j + 1)] == ".":
                     couleur = CYAN
                 print(couleur, grille[(i, j + 1)] + " ", end='')
             # GRILLE2
-            print("   ",end="")
-            print (GREEN + "{0:2d}".format(j + 1)+" | ",end="")
+            print("   ", end="")
+            print(GREEN + "{0:2d}".format(j + 1)+" | ", end="")
             for i in self.largeur_string:
-                couleur= RED               
-                if grille2[(i, j + 1)]=="X" :
+                couleur = RED
+                if grille2[(i, j + 1)] == "X":
                     couleur = BLUE
-                if grille2[(i, j + 1)]=="." :
+                if grille2[(i, j + 1)] == ".":
                     couleur = CYAN
                 print(couleur, grille2[(i, j + 1)] + " ", end='')
 
         print("\n")
         for bateau in self.list_bateaux_cpu:
-            print(bateau.long_name, " (", len(bateau.integrite), "/", bateau.taille," cases)",sep="")
-                    
+            print(bateau.long_name, " (", len(bateau.integrite),
+                  "/", bateau.taille, " cases)", sep="")
+
     def salve(self):
 
         print(WHITE + "\nEntrer votre salve: ", end="")
         tir = input()
-        #parse tir
+        # parse tir
         try:
             tir = tir.upper()
-            X=tir[0]
-            Y=int(tir[1:])
+            X = tir[0]
+            Y = int(tir[1:])
         except:
             print("Erreur de coordonnée !!!")
             return
         reponse = ""
         for bateau in self.list_bateaux_cpu:
-            if tir in bateau.position :
-                self.grille_joueur_cherche[(X, Y)]=bateau.name
+            if tir in bateau.position:
+                self.grille_joueur_cherche[(X, Y)] = bateau.name
                 reponse = RED + bateau.test_tir(tir)
         if reponse == "":
             reponse = CYAN + "A l eau..."
             self.grille_joueur_cherche[(X, Y)] = "X"
 
-        self.affiche(self.grille_joueur_cherche,self.grille_joueur_bateau)
+        self.affiche(self.grille_joueur_cherche, self.grille_joueur_bateau)
         print("")
         print(reponse)
 
@@ -138,10 +141,9 @@ if __name__ == "__main__":
 
     # import
     from colorama import init, Fore
-    init() 
+    init()
+
     # CONSTANTES
-
-
 
     from class_bateau import Bateau
     RED = Fore.RED
@@ -152,9 +154,7 @@ if __name__ == "__main__":
     WHITE = Fore.WHITE
     RESET = Fore.RESET
 
-    game = Core(12,12)
-    game.affiche(game.grille_joueur_cherche,game.grille_joueur_bateau)
+    game = Core(12, 12)
+    game.affiche(game.grille_joueur_cherche, game.grille_joueur_bateau)
     while "jeu_en_cours":
         game.salve()
-
-            
